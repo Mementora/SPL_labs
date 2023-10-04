0f28e5e8d8475669cf1a8e322cbecd6cfb56cf0c
Oleh Kuzo <olegkuzo23@gmail.com>
2023-10-04 11:49:46 +0300

    lab 2 created files

---
+++ Lab2/Solutions.py
@@ -0,0 +1,7 @@
+class Calculator:
+    def __init__(self,firstNumber, secondNumber, operator):
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+        self.operator = operator
+        
+
