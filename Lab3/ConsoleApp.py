b45f8b0ffb4738eb68e8133697901cedbe33bfa7
Oleh Kuzo <olegkuzo23@gmail.com>
2023-10-19 13:36:44 +0300

    lab3 complete

--- Lab3/ConsoleApp.py
+++ Lab3/ConsoleApp.py
@@ -3,12 +3,13 @@ from colorama import Fore, Style
 
 class ConsoleApp:
     def __init__(self):
-        self.fonts = ['standard', 'banner', 'slant', 'script']
-        self.colors = [Fore.LIGHTRED_EX, Fore.LIGHTBLUE_EX, Fore.LIGHTGREEN_EX]
+        self.fonts = ['standard', 'banner', 'slant', 'starwars']
+        self.colors = [Fore.RED, Fore.BLUE, Fore.GREEN, Fore.BLACK]
 
+    #get user input
     def get_user_input(self):
 
-        user_input = input(Fore.LIGHTYELLOW_EX + "Enter a word or phrase to convert to ASCII art: ")
+        user_input = input("Enter a word or phrase to convert to ASCII art: ")
 
         self.show_available_fonts()
         font_choice = self.get_font_choice()
@@ -26,15 +27,17 @@ class ConsoleApp:
 
         return user_input, self.fonts[font_choice - 1], self.colors[color_choice - 1], width, height, char_set, preview_enabled
 
+    #show a list of available
     def show_available_fonts(self):
         print("Available font styles:")
-        for i, font in enumerate(self.fonts, start=1):
+        for i, font in enumerate(self.fonts, start=0):
             print(f"{i}. {font}")
 
+    #get user choise of font
     def get_font_choice(self):
         while True:
             try:
-                font_choice = int(input("Choose a font style number (1-4): "))
+                font_choice = int(input("Choose a font style number (0-3): "))
                 if 1 <= font_choice <= 4:
                     return font_choice
                 else:
@@ -42,15 +45,17 @@ class ConsoleApp:
             except ValueError:
                 print("Please enter the correct font style number.")
 
+    #show a list of colors
     def show_available_colors(self):
         print("Available text colors:")
-        for i, color in enumerate(self.colors, start=1):
-            print(f"{i}. Color {i}")
+        for i, color in enumerate(self.colors, start=0):
+            print(f"{i}. Color {color}")
 
+    #get user choise of color
     def get_color_choice(self):
         while True:
             try:
-                color_choice = int(input("Choose a text color number (1-3): "))
+                color_choice = int(input(Fore.RESET + "Choose a text color number (0-3): "))
                 if 1 <= color_choice <= 3:
                     return color_choice
                 else:
@@ -58,6 +63,7 @@ class ConsoleApp:
             except ValueError:
                 print("Please enter the correct text color number.")
 
+    #using for detecting negative number
     def get_positive_number(self, prompt):
         while True:
             try:
@@ -69,12 +75,15 @@ class ConsoleApp:
             except ValueError:
                 print("Please enter the correct number.")
 
+    #get the user choise
     def get_custom_characters(self):
         return input("Enter the characters you want to use for ASCII art (e.g., '@#*'): ")
 
+    #use to preview generated ASCII art
     def preview_ascii_art(self, ascii_text, selected_color):
         print(selected_color + ascii_text + Style.RESET_ALL)
 
+    #generate ASCII art
     def generate_ascii_art(self, text, selected_font, selected_color, width, height, char_set=None):
         ascii_art = pyfiglet.Figlet(font=selected_font)
         ascii_text = ascii_art.renderText(text)
@@ -99,6 +108,7 @@ class ConsoleApp:
         colored_text = selected_color + scaled_ascii_text + Style.RESET_ALL
         return colored_text
 
+    #save ASCII art in .txt
     def save_ascii_art(self, ascii_text, file_name):
         with open(f"{file_name}.txt", "w") as file:
             file.write(ascii_text)
